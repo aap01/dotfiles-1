0ea93aa252d921f8b5335065a53ac1031d8fff62
Alif Arfab Pavel <aap.pavel@gmail.com>
2022-07-14 17:12:53 +0600

    update flake.nix Added my own machine name

--- flake.nix
+++ flake.nix
@@ -69,9 +69,10 @@
         # TODO: replace mbp2021 with your mac name
         # TODO: run this command to build config
         # nix build .#darwinConfigurations.mbp2021.system
+        # nix build .#darwinConfigurations.alif-mac.system
         # TODO: run this command to launch new build
         # ./result/sw/bin/darwin-rebuild switch --flake .
-        mbp2021 = darwin.lib.darwinSystem {
+        alif-mac = darwin.lib.darwinSystem {
           system = "aarch64-darwin";
           modules = [ ./nixpkgs/darwin/mbp2021/configuration.nix ];
           inputs = { inherit darwin nixpkgs; };
